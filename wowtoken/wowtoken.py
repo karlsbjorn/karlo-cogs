521f660a59a25bcddecb0c181f6a516e8e11370d
Karlo <karlo@karloprikratki.com>
2021-12-22 22:16:41 +0100

    Add `priceall` subcommand

--- wowtoken/wowtoken.py
+++ wowtoken/wowtoken.py
@@ -4,6 +4,8 @@ from redbot.core import Config
 from redbot.core import commands
 from blizzardapi import BlizzardApi
 
+VALID_REGIONS = ("eu", "us", "kr")
+
 
 class Wowtoken(commands.Cog):
     """Very basic cog for checking the price of the World of Warcraft token in various regions."""
@@ -18,8 +20,7 @@ class Wowtoken(commands.Cog):
 
     @token.command()
     async def price(self, ctx, region: str):
-        """Check price of WoW token"""
-        valid_regions = {"eu", "us", "kr"}
+        """Check price of WoW token in a region"""
         async with ctx.typing():
             try:
                 blizzard_api = await self.bot.get_shared_api_tokens("blizzard")
@@ -30,15 +31,43 @@ class Wowtoken(commands.Cog):
                     raise ValueError("Blizzard API nije pravilno postavljen.")
                 api_client = BlizzardApi(cid, secret)
 
-                if region not in valid_regions:
+                if region not in VALID_REGIONS:
                     raise ValueError("Neispravna regija. Ispravne regije su: `eu`, `us` i `kr`.")
 
                 fetch_token = functools.partial(api_client.wow.game_data.get_token_index, region=region, locale="en_US")
                 wow_token = await self.bot.loop.run_in_executor(None, fetch_token)
                 token_price = str(wow_token["price"])
-                message = f"Trenutna cijena {region.upper()} WoW Tokena je: **{self.format_to_gold(token_price)}**"
 
-                await ctx.send(embed=discord.Embed(description=message))
+                message = f"Trenutna cijena {region.upper()} WoW Tokena je: **{self.format_to_gold(token_price)}**"
+                embed = discord.Embed(description=message, colour=await ctx.embed_colour())
+
+                await ctx.send(embed=embed)
+            except Exception as e:
+                await ctx.send(f"Naredba uspješno neuspješna. {e}")
+
+    @token.command()
+    async def priceall(self, ctx):
+        """Check price of WoW token in all supported regions"""
+        async with ctx.typing():
+            try:
+                blizzard_api = await self.bot.get_shared_api_tokens("blizzard")
+                cid = blizzard_api.get("client_id")
+                secret = blizzard_api.get("client_secret")
+
+                if not cid or not secret:
+                    raise ValueError("Blizzard API nije pravilno postavljen.")
+                api_client = BlizzardApi(cid, secret)
+
+                embed = discord.Embed(title="WoW Token prices", colour=await ctx.embed_colour())
+
+                for region in VALID_REGIONS:
+                    fetch_token = functools.partial(
+                        api_client.wow.game_data.get_token_index, region=region, locale="en_US"
+                    )
+                    wow_token = await self.bot.loop.run_in_executor(None, fetch_token)
+                    token_price = str(wow_token["price"])
+                    embed.add_field(name=region.upper(), value=self.format_to_gold(token_price))
+                await ctx.send(embed=embed)
             except Exception as e:
                 await ctx.send(f"Naredba uspješno neuspješna. {e}")
 
