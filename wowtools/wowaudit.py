824d331274069c037367fafcbc43d0ac74271f8c
Karlo <karlo@karloprikratki.com>
2022-03-18 12:14:03 +0100

    [WoWTools] prettier embed

--- wowtools/wowaudit.py
+++ wowtools/wowaudit.py
@@ -7,13 +7,11 @@ from redbot.core.utils.menus import DEFAULT_CONTROLS
 
 import gspread_asyncio
 from google.oauth2.service_account import Credentials
-from datetime import datetime
 
 _ = Translator("WoWTools", __file__)
 
 
 class Wowaudit:
-
     @commands.group()
     async def wow(self, ctx):
         pass
@@ -30,42 +28,66 @@ class Wowaudit:
             ws = await ss.get_worksheet(0)
 
             sheet_title = await ss.get_title()
-            member_count = int((await ws.acell('H5')).value)
-            members = await ws.get_values('C9:F' + str(9 + (member_count - 1)))
+            member_count = int((await ws.acell("H5")).value)
+            members = await ws.get_values("C9:F" + str(9 + (member_count - 1)))
 
-            avg_ilvl_output = ''
+            avg_ilvl_output = ""
             embeds = []
-            page = 0
+            embed_colour = await ctx.embed_color()
+            page_n = 0
             members_per_page = 20
-            # TODO: tabulate
             for member in members:
                 member_rank = member[0]
                 member_name = member[1]
                 member_ilvl = member[3]
 
-                avg_ilvl_output += "{member_rank}. {member_name} - {member_ilvl}\n".format(member_rank=member_rank,member_name=member_name,member_ilvl=chat_formatting.bold(member_ilvl))
+                avg_ilvl_output += _(
+                    "{member_rank}. {member_name} - {member_ilvl}\n"
+                ).format(
+                    member_rank=member_rank,
+                    member_name=member_name,
+                    member_ilvl=chat_formatting.bold(member_ilvl),
+                )
                 if (int(member_rank) % members_per_page) == 0:
-                    page += 1
-                    embed = discord.Embed(title=sheet_title, timestamp=datetime.utcnow())
-                    embed.add_field(name="Average Item Level", value=avg_ilvl_output)
-                    embed.set_footer(text="Stranica " + str(page) + " - U slučaju krivih informacija obratite se @jasko")
+                    page_n += 1
+                    embed = self.gen_avg_ilvl_page(
+                        ctx, embed_colour, page_n, sheet_title, avg_ilvl_output
+                    )
                     embeds.append(embed)
-                    avg_ilvl_output = ''
-            if avg_ilvl_output != '':
-                page += 1
-                embed = discord.Embed(title=sheet_title, timestamp=datetime.utcnow())
-                embed.add_field(name="Average Item Level", value=avg_ilvl_output)
-                embed.set_footer(text="Stranica " + str(page) + " - U slučaju krivih informacija obratite se @jasko")
+                    avg_ilvl_output = ""
+            if avg_ilvl_output != "":
+                page_n += 1
+                embed = self.gen_avg_ilvl_page(
+                    ctx, embed_colour, page_n, sheet_title, avg_ilvl_output
+                )
                 embeds.append(embed)
 
             await menus.menu(ctx, embeds, DEFAULT_CONTROLS)
 
+    @staticmethod
+    def gen_avg_ilvl_page(
+        ctx, colour, page_n, sheet_title, avg_ilvl_output
+    ) -> discord.Embed:
+        embed = discord.Embed(
+            colour=colour,
+        )
+        embed.set_author(name=sheet_title, icon_url=ctx.guild.icon_url)
+        embed.add_field(name=_("Average Item Level"), value=avg_ilvl_output)
+        embed.set_footer(
+            text=_(
+                "Stranica {page_number} - U slučaju krivih informacija obratite se @jasko"
+            ).format(page_number=page_n)
+        )
+        return embed
+
     def get_creds(self):
         creds_path = str(data_manager.cog_data_path(self)) + "/service_account.json"
         creds = Credentials.from_service_account_file(creds_path)
-        scoped = creds.with_scopes([
-            "https://spreadsheets.google.com/feeds",
-            "https://www.googleapis.com/auth/spreadsheets",
-            "https://www.googleapis.com/auth/drive",
-        ])
+        scoped = creds.with_scopes(
+            [
+                "https://spreadsheets.google.com/feeds",
+                "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive",
+            ]
+        )
         return scoped
