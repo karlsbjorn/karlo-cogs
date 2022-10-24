c8116c997c17782fed0ebdc378b03433950206cf
Karlo <karlo@karloprikratki.com>
2022-10-24 15:30:21 +0200

    [DiscordStreams] many fixes

--- discordstreams/discordstreams.py
+++ discordstreams/discordstreams.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, List
 
 import discord
@@ -5,6 +6,7 @@ from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.i18n import Translator, cog_i18n
 
+log = logging.getLogger("red.karlo-cogs.discordstreams")
 _ = Translator("DiscordStreams", __file__)
 
 
@@ -12,7 +14,9 @@ _ = Translator("DiscordStreams", __file__)
 class DiscordStreams(commands.Cog):
     def __init__(self, bot):
         self.bot: Red = bot
-        self.config = Config.get_conf(self, identifier=87446677010550784, force_registration=True)
+        self.config = Config.get_conf(
+            self, identifier=87446677010550784, force_registration=True
+        )
         default_guild = {
             "alert_channels": [],
             "active_messages": {},
@@ -21,7 +25,9 @@ class DiscordStreams(commands.Cog):
 
     @commands.command()
     @commands.guild_only()
-    async def golivealert(self, ctx: commands.Context, channel: discord.TextChannel = None):
+    async def golivealert(
+        self, ctx: commands.Context, channel: discord.TextChannel = None
+    ):
         """Set the channel for live alerts."""
         if channel is None:
             channel = ctx.channel
@@ -35,69 +41,132 @@ class DiscordStreams(commands.Cog):
                     channel=channel.mention
                 )
             )
+            await self._clean_up_config(channel, ctx)
         else:
             alert_channels.append(channel.id)
             await self.config.guild(ctx.guild).alert_channels.set(alert_channels)
             await ctx.send(
-                _("Go Live alerts will be sent to {channel}").format(channel=channel.mention)
+                _("Go Live alerts will be sent to {channel}").format(
+                    channel=channel.mention
+                )
             )
 
     @commands.Cog.listener("on_voice_state_update")
     async def on_voice_state_update(
-        self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
     ):
         member_guild: discord.Guild = member.guild
         guild_config: Dict = await self.config.guild(member_guild).all()
         enabled = bool(guild_config["alert_channels"])
 
-        if not enabled or await self.bot.cog_disabled_in_guild(self, member_guild) or member.bot:
+        if (
+            not enabled
+            or await self.bot.cog_disabled_in_guild(self, member_guild)
+            or member.bot
+        ):
             return
 
         # Stream ended
         if not after.self_stream:
             active_messages: Dict = guild_config["active_messages"]
             member_id = str(member.id)
-            if member_id not in active_messages.keys():
+            if member_id not in active_messages:
                 return
 
-            alert_channels: List = guild_config["alert_channels"]
-            for channel_id in alert_channels:
-                channel: discord.TextChannel = member_guild.get_channel(channel_id)
-                if not channel:  # Channel was deleted
-                    channels = alert_channels
-                    channels.remove(channel_id)
-                    await self.config.guild(member_guild).alert_channels.set(channels)
-                    continue
-
-                live_message = active_messages[member_id]
-                try:
-                    message: discord.Message = await channel.fetch_message(live_message)
-                    await message.delete()
-                except discord.NotFound:  # Message was already deleted
-                    messages: Dict = guild_config["active_messages"]
-                    messages.pop(member_id)
-                    await self.config.guild(member_guild).active_messages.set(messages)
+            await self._remove_stream_alerts(
+                active_messages, guild_config, member_guild, member_id
+            )
 
         # Stream started
         if (not before.self_stream) and after.self_stream:
-            channels_to_send_to = await self.config.guild(member_guild).alert_channels()
-            stream = DiscordStream(after.channel, member)
+            await self._send_stream_alerts(member, after)
 
-            embed = stream.make_embed()
-            for channel_id in channels_to_send_to:
-                channel: discord.TextChannel = member_guild.get_channel(channel_id)
-                if channel is None:
-                    channels_to_send_to.remove(channel_id)
-                    await self.config.guild(member_guild).alert_channels.set(channels_to_send_to)
-                    continue
+    async def _remove_stream_alerts(
+        self, active_messages, guild_config, member_guild, member_id
+    ) -> None:
+        """
+        Remove the stream alerts for a member.
 
-                message = await channel.send(
-                    content=_("{member} is live!").format(member=member.display_name), embed=embed
+        :param active_messages: The active alert messages for the guild.
+        :param guild_config: The guild's config info.
+        :param member_guild: The guild the member is in.
+        :param member_id: The ID of the member.
+        :return:
+        """
+        alert_channels: List = guild_config["alert_channels"]
+        for channel_id in alert_channels:
+            channel: discord.TextChannel = member_guild.get_channel(channel_id)
+            if not channel:  # Channel was deleted
+                channels = alert_channels
+                channels.remove(channel_id)
+                await self.config.guild(member_guild).alert_channels.set(channels)
+                continue
+
+            try:
+                message_id = active_messages[member_id][str(channel_id)]["message"]
+            except KeyError:
+                log.info("Channel ID was provided after stream started. Ignoring.")
+                continue
+
+            try:
+                message: discord.Message = await channel.fetch_message(message_id)
+                await message.delete()
+                active_messages[member_id].pop(str(channel_id))
+            except discord.NotFound:  # Message was already deleted
+                active_messages[member_id].pop(str(channel_id))
+        await self.config.guild(member_guild).active_messages.set(active_messages)
+
+    async def _send_stream_alerts(
+        self, member: discord.Member, after: discord.VoiceState
+    ) -> None:
+        """
+        Send a message to the alert channels when a member starts streaming.
+
+        :param member: The member who started streaming.
+        :param after: The member's voice state after starting their stream.
+        :return: None
+        """
+        member_guild: discord.Guild = member.guild
+        channels_to_send_to = await self.config.guild(member_guild).alert_channels()
+
+        stream = DiscordStream(after.channel, member)
+        embed = stream.make_embed()
+
+        active_messages = {}
+        for channel_id in channels_to_send_to:
+            channel: discord.TextChannel = member_guild.get_channel(channel_id)
+            if channel is None:
+                channels_to_send_to.remove(channel_id)
+                await self.config.guild(member_guild).alert_channels.set(
+                    channels_to_send_to
                 )
-                await self.config.guild(member_guild).active_messages.set_raw(
-                    str(member.id), value=message.id
-                )
-        return
+                continue
+
+            message = await channel.send(
+                content=_("{member} is live!").format(member=member.display_name),
+                embed=embed,
+            )
+
+            member_id = str(member.id)
+            channel_id = str(channel_id)
+
+            # Make the keys if they don't exist
+            if member_id not in active_messages:
+                active_messages[member_id] = {}
+            if channel_id not in active_messages[member_id]:
+                active_messages[member_id][channel_id] = {}
+
+            active_messages[member_id][channel_id]["message"] = message.id
+
+        await self.config.guild(member_guild).active_messages.set(active_messages)
+
+    async def _clean_up_config(self, channel, ctx):
+        active_messages: Dict = await self.config.guild(ctx.guild).active_messages()
+        active_messages[str(ctx.author.id)].pop(str(channel.id))
+        await self.config.guild(ctx.guild).active_messages.set(active_messages)
 
 
 class DiscordStream:
