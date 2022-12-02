3014fb2537017c1697592f3e0e50c832fd9801a7
Karlo <karlo@karloprikratki.com>
2022-12-02 18:13:19 +0100

    update to dragonflight s1 raid

--- wowtools/raiderio.py
+++ wowtools/raiderio.py
@@ -98,7 +98,7 @@ class Raiderio:
                 ]["all"]
                 char_score_color = int("0x" + char_score["color"][1:], 0)
                 char_raid = profile_data["raid_progression"][
-                    "sepulcher-of-the-first-ones"
+                    "vault-of-the-incarnates"
                 ]["summary"]
                 char_last_updated = self._parse_date(profile_data["last_crawled_at"])
                 char_ilvl = profile_data["gear"]["item_level_equipped"]
