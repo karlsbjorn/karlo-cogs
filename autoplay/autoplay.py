20853115eb58ba34e160f8d4de374377c20d2815
Karlo <karlo@karloprikratki.com>
2023-01-12 17:23:29 +0100

    join channel on autoplay

--- autoplay/autoplay.py
+++ autoplay/autoplay.py
@@ -19,7 +19,7 @@ class AutoPlay(commands.Cog):
     """Automatically play music that a guild member is listening to on Spotify."""
 
     def __init__(self, bot: DISCORD_BOT_TYPE):
-        self.bot: Red = bot
+        self.bot: DISCORD_BOT_TYPE = bot
         self.config = Config.get_conf(self, identifier=87446677010550784, force_registration=True)
         default_guild = {"tracked_member": None, "autoplaying": False, "paused_track": None}
         self.config.register_guild(**default_guild)
@@ -48,6 +48,7 @@ class AutoPlay(commands.Cog):
                     description=_("I'll no longer autoplay."), messageable=ctx
                 )
             )
+            return
         else:
             await self.config.guild(ctx.guild).tracked_member.set(member.id)
             await ctx.send(
@@ -58,6 +59,8 @@ class AutoPlay(commands.Cog):
                     messageable=ctx,
                 )
             )
+
+        await self._prepare_autoplay(ctx.guild, ctx.author)
 
     async def _context_user_autoplay(
         self, interaction: DISCORD_INTERACTION_TYPE, member: discord.Member
@@ -82,6 +85,21 @@ class AutoPlay(commands.Cog):
                 messageable=interaction,
             )
         )
+
+        await self._prepare_autoplay(interaction.guild, interaction.user)
+
+    async def _prepare_autoplay(self, guild, author):
+        player: Player = self.bot.pylav.get_player(guild.id)
+
+        if not player and author.voice:
+            await self.bot.pylav.connect_player(author, author.voice.channel)
+        elif player and player.channel.id != author.voice.channel.id:
+            await player.move_to(author, author.voice.channel)
+
+        if player.is_playing or player.paused:
+            await player.stop(author)
+        if player.queue.size:
+            player.queue.clear()
 
     @commands.Cog.listener("on_presence_update")
     async def _on_presence_update(
@@ -123,7 +141,7 @@ class AutoPlay(commands.Cog):
         query = await Query.from_string(current_activity.track_url)
         response = await self.bot.lavalink.search_query(query=query)
         if response is None or not response.tracks:
-            log.debug("No tracks found.")
+            log.verbose(f"No tracks found. Response: {response}")
             return
 
         log.verbose(f"Query successful: {response.tracks[0]}")
@@ -190,7 +208,8 @@ class AutoPlay(commands.Cog):
         if interaction.type != discord.InteractionType.application_command:
             return
         log.verbose(
-            f"Interaction {interaction.type}, {interaction.command.name if interaction.command else None} used."
+            f"Interaction {interaction.type}, "
+            f"{interaction.command.name if interaction.command else None} used."
         )
         player_commands = [
             "play",
