388b4905ea0b042d66cc199a28cf84ac4b52f3fc
Karlo <karlo@karloprikratki.com>
2023-02-25 04:07:52 +0100

    replace WarcraftlogsRetail with dpy2 version

--- warcraftlogsretail/encounterid.py
+++ warcraftlogsretail/encounterid.py
@@ -1,79 +1,25 @@
 ENCOUNTER_ID = {
-    # Castle Nathria
-    2398: "Shriekwing",
-    2418: "Huntsman Altimor",
-    2383: "Hungering Destroyer",
-    2402: "Sun King's Salvation",
-    2405: "Artificer Xy'mox",
-    2406: "Lady Inerva Darkvein",
-    2412: "The Council of Blood",
-    2399: "Sludgefist",
-    2417: "Stone Legion Generals",
-    2407: "Sire Denathrius",
-    # Sanctum of Domination
-    2423: "The Tarragrue",
-    2433: "The Eye of the Jailer",
-    2429: "The Nine",
-    2432: "Remnant of Ner'zhul",
-    2434: "Soulrender Dormazain",
-    2430: "Painsmith Raznal",
-    2436: "Guardian of the First Ones",
-    2431: "Fatescribe Roh-Kalo",
-    2422: "Kel'Thuzad",
-    2435: "Sylvanas Windrunner",
-    # Sepulcher of the First Ones
-    2512: "Vigilant Guardian",
-    2540: "Dausegne, the Fallen Oracle",
-    2553: "Artificer Xy'mox",
-    2544: "Prototype Pantheon",
-    2542: "Skolex, the Insatiable Ravener",
-    2529: "Halondrus the Reclaimer",
-    2539: "Lihuvim, Principal Architect",
-    2546: "Anduin Wrynn",
-    2543: "Lords of Dread",
-    2549: "Rygelon",
-    2537: "The Jailer",
+    # Vault of the Incarnates
+    2587: "Eranog",
+    2639: "Terros",
+    2590: "The Primal Council",
+    2592: "Sennarth, The Cold Breath",
+    2635: "Dathea, Ascended",
+    2605: "Kurog Grimtotem",
+    2614: "Broodkeeper Diurna",
+    2607: "Raszageth the Storm-Eater",
 }
 
 ENCOUNTER_ZONES = {
-    "Castle Nathria": [2398, 2418, 2383, 2402, 2405, 2406, 2412, 2399, 2417, 2407],
-    "Sanctum of Domination": [  # wtf black
-        2423,
-        2433,
-        2429,
-        2432,
-        2434,
-        2430,
-        2436,
-        2431,
-        2422,
-        2435,
-    ],
-    "Sepulcher of the First Ones": [
-        2512,
-        2540,
-        2553,
-        2544,
-        2542,
-        2529,
-        2539,
-        2546,
-        2543,
-        2549,
-        2537,
-    ],
+    "Vault of the Incarnates": [2587, 2639, 2590, 2592, 2635, 2605, 2614, 2607],
 }
 
 ZONES_BY_ID = {
-    26: "Castle Nathria",
-    28: "Sanctum of Domination",
-    29: "Sepulcher of the First Ones",
+    31: "Vault of the Incarnates",
 }
 
 ZONES_BY_SHORT_NAME = {
-    "CN": ["Castle Nathria", 26],
-    "SOD": ["Sanctum of Domination", 28],
-    "SOTFO": ["Sepulcher of the First Ones", 29],
+    "VOTI": ["Vault of the Incarnates", 31],
 }
 
 DIFFICULTIES = {
