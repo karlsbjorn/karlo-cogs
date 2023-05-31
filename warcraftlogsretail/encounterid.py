9800ad476aa0ff24864fae6b38aee3341d2940ea
Karlo Prikratki <karlo@karloprikratki.com>
2023-05-31 18:22:47 +0200

    [WarcraftLogsRetail] update for s2

--- warcraftlogsretail/encounterid.py
+++ warcraftlogsretail/encounterid.py
@@ -8,18 +8,31 @@ ENCOUNTER_ID = {
     2605: "Kurog Grimtotem",
     2614: "Broodkeeper Diurna",
     2607: "Raszageth the Storm-Eater",
+    # Aberrus, the Shadowed Crucible
+    2688: "Kazzara, the Hellforged",
+    2687: "The Amalgamation Chamber",
+    2693: "The Forgotten Experiments",
+    2682: "Assault of the Zaqali",
+    2680: "Rashok, the Elder",
+    2689: "Zskarn, the Vigilant Steward",
+    2683: "Magmorax",
+    2684: "Echo of Neltharion",
+    2685: "Scalecommander Sarkareth",
 }
 
 ENCOUNTER_ZONES = {
     "Vault of the Incarnates": [2587, 2639, 2590, 2592, 2635, 2605, 2614, 2607],
+    "Aberrus, the Shadowed Crucible": [2688, 2687, 2693, 2682, 2680, 2689, 2683, 2684, 2685],
 }
 
 ZONES_BY_ID = {
     31: "Vault of the Incarnates",
+    33: "Aberrus, the Shadowed Crucible",
 }
 
 ZONES_BY_SHORT_NAME = {
     "VOTI": ["Vault of the Incarnates", 31],
+    "ASC": ["Aberrus, the Shadowed Crucible", 33],
 }
 
 DIFFICULTIES = {
