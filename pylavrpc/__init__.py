b3dc8f4ea85bcb9b964f2532bc03351bba72398e
Karlo Prikratki <karlo@karloprikratki.com>
2024-04-16 18:15:02 +0200

    `pylav_auto_setup`

--- pylavrpc/__init__.py
+++ pylavrpc/__init__.py
@@ -1,5 +1,13 @@
+import json
+from pathlib import Path
+
+from pylav.extension.red.utils.required_methods import pylav_auto_setup
+
 from .pylavrpc import PyLavRPC
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot):
-    await bot.add_cog(PyLavRPC(bot))
+    await pylav_auto_setup(bot, PyLavRPC)
