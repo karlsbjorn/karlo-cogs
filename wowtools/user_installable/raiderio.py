69bf77e196f81628216825a56b127f1684e5bafd
Karlo Prikratki <karlo@karloprikratki.com>
2024-07-20 10:38:13 +0200

    description

--- wowtools/user_installable/raiderio.py
+++ wowtools/user_installable/raiderio.py
@@ -18,6 +18,7 @@ _ = Translator("WoWTools", __file__)
 class UserInstallableRaiderio:
     user_install_raiderio = app_commands.Group(
         name="raiderio",
+        description=_("Raider.io commands"),
         allowed_installs=AppInstallationType(guild=True, user=True),
         allowed_contexts=AppCommandContext(guild=True, dm_channel=True, private_channel=True),
     )
