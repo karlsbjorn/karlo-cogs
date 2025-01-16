77c76a5d4a4e026f2ccea8459487da1aa4913466
Karlo Prikratki <karlo@karloprikratki.com>
2025-01-16 06:21:12 +0100

    critical-only setting

--- netdataalerts/netdata_alerts.py
+++ netdataalerts/netdata_alerts.py
@@ -21,6 +21,8 @@ class NetdataAlerts(commands.Cog):
         self.session = aiohttp.ClientSession(
             headers={"User-Agent": "Red-DiscordBot/karlo-cogs/NetdataAlertsCog"}
         )
+        default_global = {"only_critical": False}
+        self.config.register_global(**default_global)
         self.netdata_base = self.form_netdata_base()
         self.prev_alarms: set[int] = set()
         self.current_alarms: set[int] = set()
@@ -43,6 +45,27 @@ class NetdataAlerts(commands.Cog):
             raise NetdataNotRunning
         self.check_alarms.start()
 
+    @commands.group()
+    @commands.is_owner()
+    async def netdataset(self, ctx: commands.Context):
+        """Change NetdataAlerts settings."""
+        pass
+
+    @netdataset.command(name="critical")
+    async def netdataset_critical(self, ctx: commands.Context):
+        only_critical = await self.config.only_critical()
+        if only_critical:
+            await self.config.only_critical.set(False)
+            await ctx.send(
+                _("NetdataAlerts will now send notifications for all alerts"), silent=True
+            )
+        else:
+            await self.config.only_critical.set(True)
+            await ctx.send(
+                _("NetdataAlerts will now send notifications only for critical alerts"),
+                silent=True,
+            )
+
     # The server is local so there's no issue spamming the endpoint
     @tasks.loop(seconds=5)
     async def check_alarms(self):
@@ -56,6 +79,8 @@ class NetdataAlerts(commands.Cog):
             alarm = Alarm.model_validate(alarm_data)
             if alarm.id in self.prev_alarms:
                 self.current_alarms.add(alarm.id)
+                continue
+            if alarm.status != "CRITICAL" and await self.config.only_critical():
                 continue
             embeds.append(
                 discord.Embed(
@@ -71,7 +96,7 @@ class NetdataAlerts(commands.Cog):
             return
         destinations = await self.bot.get_owner_notification_destinations()
         for dest in destinations:
-            await dest.send(embeds=embeds, silent=critical)
+            await dest.send(embeds=embeds, silent=not critical)
 
     def make_description(self, alarm: Alarm) -> str:
         return f"{alarm.info}\nValue: {alarm.value_string}\n<t:{alarm.last_status_change}:R>"
