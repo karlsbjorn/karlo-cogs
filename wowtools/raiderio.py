78750d21fe0c50811ec1fe3821141b8cc7de31cf
Karlo Prikratki <karlo@karloprikratki.com>
2025-04-28 20:55:36 +0200

    fix

--- wowtools/raiderio.py
+++ wowtools/raiderio.py
@@ -25,6 +25,7 @@ class Raiderio:
 
     @raiderio.command(name="profile")
     @commands.guild_only()
+    @commands.bot_has_permissions(embed_links=True)
     @app_commands.describe(
         character="Name of the character to look up",
         realm="The character's realm",
