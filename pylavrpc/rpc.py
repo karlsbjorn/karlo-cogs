c613e274d5e9b38630eac45fdc79b54f1f027554
Karlo Prikratki <karlo@karloprikratki.com>
2025-05-30 22:11:12 +0200

    pylavrpc better error handling

--- pylavrpc/rpc.py
+++ pylavrpc/rpc.py
@@ -1,9 +1,13 @@
+from typing import Any
+
+
+import discord
 from pylav.players.player import Player
 from pylav.players.query.obj import Query
 
 
 class RPC:
-    async def get_current_track(self, guild_id: int):
+    async def get_current_track(self, guild_id: int) -> dict[str, Any]:
         guild = self.bot.get_guild(guild_id)
         if not guild:
             return {"status": 404, "message": "Guild not found."}
@@ -18,11 +22,11 @@ class RPC:
 
         return await current_track.to_dict()
 
-    async def play_track(self, guild_id: int, query: str):
+    async def play_track(self, guild_id: int, query: str) -> dict[str, Any]:
         if not query:
             return {"status": 400, "message": "No query provided."}
         
-        guild = self.bot.get_guild(guild_id)
+        guild: discord.Guild = self.bot.get_guild(guild_id) # type: ignore
         if not guild:
             return {"status": 404, "message": "Guild not found."}
 
@@ -30,8 +34,10 @@ class RPC:
         if not player:
             return {"status": 400, "message": "Bot not connected to voice channel."}
 
-        query = await Query.from_string(query)
-        response = await self.bot.lavalink.search_query(query=query)
+        query_obj: Query = await Query.from_string(query)
+        if query_obj.is_album or query_obj.is_playlist:
+            return {"status": 400, "message": "Only single tracks allowed on this endpoint."}
+        response = await self.bot.lavalink.search_query(query=query_obj)
         if response.loadType == "error":
             return {"status": 400, "message": "Error getting track"}
 
@@ -47,7 +53,7 @@ class RPC:
             "message": "Error playing track."
         }
 
-    async def play_next(self, guild_id: int):
+    async def play_next(self, guild_id: int) -> dict[str, Any]:
         guild = self.bot.get_guild(guild_id)
         if not guild:
             return {"status": 404, "message": "Guild not found."}
