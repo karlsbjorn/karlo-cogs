501d0a6ef2f5b49e8bf8dc719a90bc8a5bf1a834
Karlo Prikratki <karlo@karloprikratki.com>
2026-08-19 16:22:44 +0200

    cosmetic

--- wowtools/scoreboard.py
+++ wowtools/scoreboard.py
@@ -647,7 +647,7 @@ class Scoreboard:
         latest_scores = {
             char: cache[max(cache.keys())] for char, cache in score_cache.items() if cache
         }
-        top = sorted(latest_scores, key=lambda c: latest_scores[c], reverse=True)[:20]
+        top = sorted(latest_scores, key=lambda c: latest_scores[c], reverse=True)[:15]
 
         fig = go.Figure()
 
@@ -668,7 +668,7 @@ class Scoreboard:
                     name=char_name,
                     text=[""] * (len(scores) - 1) + [char_name],  # label on last
                     textposition="middle right",
-                    textfont=dict(size=18),
+                    textfont=dict(size=24),
                     line=dict(width=4),
                     cliponaxis=False,
                 )
